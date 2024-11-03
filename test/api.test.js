ffacfae35f12ec524eaf8f525744fadc0f5dd024
Wilton Guilherme <wiltoninfra@gmail.com>
2024-11-03 19:10:38 -0300

    Update files and comments

--- test/api.test.js
+++ test/api.test.js
@@ -6,23 +6,23 @@ describe('API Tests', () => {
   let testItemId;
 
   // Teste para obter todos os itens (deve estar vazio no início)
-  it('should get all items (initially empty)', async () => {
+  it('Retorna todos os itens (inicialmente vazio)', async () => {
     const res = await request(app).get('/items');
     expect(res.status).to.equal(200);
     expect(res.body).to.be.an('array').that.is.empty;
   });
 
   // Teste para criar um novo item
-  it('should create a new item', async () => {
+  it('Cria um novo item', async () => {
     const res = await request(app).post('/items').send({ name: 'Test Item' });
     expect(res.status).to.equal(201);
     expect(res.body).to.have.property('id');
     expect(res.body).to.have.property('name', 'Test Item');
-    testItemId = res.body.id; // Salva o ID do item para os próximos testes
+    testItemId = res.body.id; 
   });
 
   // Teste para obter um item por ID
-  it('should get an item by ID', async () => {
+  it('Obtem um item por ID', async () => {
     const res = await request(app).get(`/items/${testItemId}`);
     expect(res.status).to.equal(200);
     expect(res.body).to.have.property('id', testItemId);
@@ -30,7 +30,7 @@ describe('API Tests', () => {
   });
 
   // Teste para atualizar um item por ID
-  it('should update an item by ID', async () => {
+  it('Atualiza item por ID', async () => {
     const res = await request(app)
       .put(`/items/${testItemId}`)
       .send({ name: 'Updated Item' });
@@ -40,14 +40,14 @@ describe('API Tests', () => {
   });
 
   // Teste para deletar um item por ID
-  it('should delete an item by ID', async () => {
+  it('Exclui um item por ID', async () => {
     const res = await request(app).delete(`/items/${testItemId}`);
     expect(res.status).to.equal(200);
     expect(res.body).to.have.property('id', testItemId);
   });
 
   // Teste para verificar que o item excluído não é encontrado
-  it('should return 404 when trying to get a deleted item', async () => {
+  it('Retorna 404 ao tentar obter um item excluído', async () => {
     const res = await request(app).get(`/items/${testItemId}`);
     expect(res.status).to.equal(404);
     expect(res.body).to.have.property('message', 'Item não encontrado');
